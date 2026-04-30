ca756d095b0b6311111991a443cdab54c80b0515
lumii-luna <lunasporne@gmail.com>
2026-04-30 18:39:06 +0930

    seed toggle

--- isometric-survival-game/Scenes/main.gd
+++ isometric-survival-game/Scenes/main.gd
@@ -1,20 +1,23 @@
 extends Node2D
 
 @onready var seed_scene = preload("res://2DInventory/items/seed_item.tscn")
+@onready var player = $Player
 var seeds : Array[Node] = []
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("spawnSeed"):
-		spawn_seed()
+	if Input.is_action_just_pressed("toggleSeed"):
+		if seeds == []:
+			spawn_seed()
+		else:
+			destroy_seed(seeds.size()-1)
 
 func spawn_seed():
 	var seed = seed_scene.instantiate()
-	print(seed.position)
+	seed.position = Vector2(player.position.x + 32, player.position.y)
 	seeds.append(seed)
 	seed.seed_number = seeds.size()
 	add_child(seed)
 
 func destroy_seed(index):
-	if 0 <= index < seeds.size():
-		seeds[index].queue_free()
-		seeds.remove_at(index)
+	seeds[index].queue_free()
+	seeds.remove_at(index)
